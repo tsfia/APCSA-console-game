b82468404099076c3f819dfca52b1b29b59179a9
Isaac Sung <isaacs185@nycstudents.net>
2026-01-16 13:19:22 -0500

    Worked on Melee.java

--- Classes/Melee.java
+++ Classes/Melee.java
@@ -1,9 +1,33 @@
-public abstract class Player{
+public abstract class Melee implements Player{
     private double HP, maxHP;
     private int speed, maxSpeed;
     private String statfx1, statfx2;
-    void attack1(Player target);
-    void attack2(Player target);
-    void ultimate(Player target);
-    void dodge();
+    public double getHP(){
+        return HP;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+    public String getStatus1(){
+        return statfx1;
+    }
+    public String getStatus2(){
+        return statfx2;
+    }
+    public static void attack1(Player target){
+        target.HP -= 10;
+    }
+    public static void attack2(Player target){
+        target.HP -= 20;
+    }
+    public static void ultimate(Player target){
+        target.HP -= 100;
+    }
+    public static void dodge(){
+
+    }
+    public static void inflictBleed(Player target){
+        if (!target.getStatus1().equals(null)) target.statfx1 = "Bleed";
+
+    }
 }
