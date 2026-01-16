c49094f7746158b76307ca58fd1008b8da7d4bfb
Isaac Sung <isaacs185@nycstudents.net>
2026-01-16 13:04:22 -0500

    Abstract done

---
+++ Classes/Melee.java
@@ -0,0 +1,9 @@
+public abstract class Player{
+    private double HP, maxHP;
+    private int speed, maxSpeed;
+    private String statfx1, statfx2;
+    void attack1(Player target);
+    void attack2(Player target);
+    void ultimate(Player target);
+    void dodge();
+}
