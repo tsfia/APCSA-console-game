401ddfad3f8b9a31409cd7bcd73ac2f36ed75238
tasfia <148790271+tsfia@users.noreply.github.com>
2026-01-19 22:43:27 -0500

    Update Player.java

--- Classes/Player.java
+++ Classes/Player.java
@@ -1,7 +1,7 @@
 public abstract class Player{
     private double HP, maxHP;
     private int speed, maxSpeed;
-    private String statfx1, statfx2;
+    private String statfx1, statfx2, ultimate;
     void attack1(Player target);
     void attack2(Player target);
     void ultimate(Player target);
